1094df3d8042e5f446f5ad32b1ce34aee9ade821
773-Jordy <singh.jordan88@gmail.com>
2022-10-05 08:20:12 -0700

    initialized server file

---
+++ db/client.js
@@ -0,0 +1,11 @@
+const { Pool } = require('pg');
+
+const connectionString = process.env.DATABASE_URL || 'https://localhost:5432/pokestop';
+
+const client = new Pool({
+  connectionString,
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
+});
+
+module.exports = client;
+
---
+++ db/seedData.js
@@ -0,0 +1,18 @@
+const { } = require('./');
+const client = require('./client');
+
+async function rebuildDB(){
+    console.log('Hello World');
+}
+
+
+
+
+
+
+
+
+
+module.exports = {
+    rebuildDB,
+}
