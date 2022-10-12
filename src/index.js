8ebb825f0f47b6ae67de757525188a3aee29fada
CJ <cjfritz9@gmail.com>
2022-10-12 13:53:21 -0600

    testing something really quickly

--- src/index.js
+++ src/index.js
@@ -69,7 +69,7 @@ const App = () => {
           }
         ></Route>
         <Route
-          path="/products/details"
+          path="/products/:productId"
           element={
             <Details
               // chosenCard={chosenCard}
