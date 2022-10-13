a213a4c3dff75227ee3d10686dd17ddbef107ca4
Sang <sangalee92@gmail.com>
2022-10-13 12:11:13 -0500

    Added more cards to our seed database, trying to figure out how to implement the pikachu gif

--- src/components/Thanks.js
+++ src/components/Thanks.js
@@ -1,13 +1,17 @@
 import { useState } from "react";
 import { Navigate, Link } from "react-router-dom";
 //import '../stylesheets/Register.css'
-
-
+// import Pikachu.webp from "../"
 const Thanks = () => {
-
-    return (
-        <h1>Thank you for your order. Hope to see you soon!</h1>
-    )
-}
+  return (
+    <div>
+      <h1>
+        Thank you for your order. Our carrier Pikachu is on it's way with your
+        order!
+      </h1>
+      <img src="%PUBLIC_URL%/Pikachu.webp" />
+    </div>
+  );
+};
 
 export default Thanks;
